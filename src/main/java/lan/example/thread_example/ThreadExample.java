c610a635dd6c45e51a0aa64bde34743a6fa7ff82
Pavel Turlachev <turchev@gmail.com>
2016-08-22 14:19:56 +0400

    commit Пн. авг. 22 14:19:56 SAMT 2016

--- src/main/java/lan/example/thread_example/ThreadExample.java
+++ src/main/java/lan/example/thread_example/ThreadExample.java
@@ -11,23 +11,18 @@ public final class ThreadExample extends Thread {
     public void run() {
 
         while (true) {
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(ThreadExample.class.getName()).log(Level.SEVERE, null, ex);
-            }
 
             if (INSTANCE_TEST_THREAD.getCurentThreadId() == this.getId()) {
                 
                 // Раскомментируй следующую строку
-                //INSTANCE_TEST_THREAD.incremenCurentThreadId();
+                INSTANCE_TEST_THREAD.incremenCurentThreadId();
 
                 System.out.println("Print " + this.getName()
                         + " ### ID:" + this.getId()
                         + " ### getCurentThreadId: " + INSTANCE_TEST_THREAD.getCurentThreadId());
 
                 // Закоментируй следующую строку
-                INSTANCE_TEST_THREAD.incremenCurentThreadId();          
+                //INSTANCE_TEST_THREAD.incremenCurentThreadId();          
 
                 break;
             }
